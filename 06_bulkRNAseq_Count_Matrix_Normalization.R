17b9a6fde47a181408bf176887da353c04587cc0
darshanjadhav <36076405+darshanjadhav@users.noreply.github.com>
2024-03-03 22:15:32 -0500

    .
    Update and rename 06_bulkRNAseq_Count_Matrix_Normalization.Rmd to 06_bulkRNAseq_Count_Matrix_Normalization.R

--- 06_bulkRNAseq_Count_Matrix_Normalization.R
+++ 06_bulkRNAseq_Count_Matrix_Normalization.R
@@ -1,58 +1,34 @@
----
-title: "Transporter Location - Count Matrix Normalization (DESeq)"
-author: "Ronghao Zhang"
-date: "`r Sys.Date()`"
-output:
-    rmdformats::readthedown:
-      code_folding: show
-      highlight: kate
-      number_sections: TRUE
-      thumbnails: FALSE
-      fig_width: 8
----
-
-
 # Preliminaries
-```{r setup, echo=FALSE, cache=FALSE, warning=FALSE}
 # Packages for Preliminaries 
 library(knitr)
 library(rmdformats)
-## Global options
+# Global options
 options(max.print="75")
 opts_chunk$set(comment=NA)
 opts_knit$set(width=75)
-```
 
-## R Packages
-This code chunk load up the necessary R packages to manage the data and finish Homework 01. 
-```{r load_packages, message=FALSE, warning=FALSE}
+# R Packages
+# This code chunk loads up the necessary R packages to manage the data and finish Homework 01. 
 library(DESeq2)
 library(annotables)
 library(tidyverse)
-```
 
-## Themes
-This code chunk set the theme of plots. 
-```{r plot_theme, message=FALSE}
+# Themes
+# This code chunk sets the theme of plots. 
 theme_set(theme_bw())
 plot_theme <- theme(text=element_text(size=12, family="serif")) + 
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
         panel.background = element_blank())
-```
 
 # Initialization
-```{r load_matrices}
+# Loading the normalized and filtered counts data.
 load(file = "./../00_Inputs/Normalized_Filtered_Counts.RData")
-```
 
 # Log2 Transformation
-```{r}
+# Applying log2 transformation to the normalized counts and saving the result.
 norm_dist_count <- log2(norm_count+1)
 write.csv(norm_dist_count,"./../02_Results/norm_dist_counts.csv",row.names = TRUE)
-```
 
 # Session Info
-```{r}
+# Displaying session info for reproducibility.
 sessionInfo()
-```
-
